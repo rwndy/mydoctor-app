7029d512c897dadaf2c3b1e507a7efa494807e4c
rwndy <rwndy@gmail.com>
2022-05-21 02:31:31 +0700

    added icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,10 +2,22 @@ import IconBack from './arrow-back.svg'
 import IconLightBack from './icon-light-arrow-back.svg'
 import IconAddBtn from './icon-add-btn.svg'
 import IconRemoveBtn from './icon-remove-btn.svg'
+import IconDoctor from './icon-doctor.svg'
+import IconDoctorActive from './icon-doctor-active.svg'
+import IconMessage from './icon-message.svg'
+import IconMessageActive from './icon-messages-active.svg'
+import iconMap from './icon-map.svg'
+import IconMapActive from './icon-map-active.svg'
 
 export {
   IconBack,
   IconLightBack,
   IconAddBtn,
   IconRemoveBtn,
+  IconDoctor,
+  IconDoctorActive,
+  IconMapActive,
+  IconMessage,
+  IconMessageActive,
+  iconMap
 }
